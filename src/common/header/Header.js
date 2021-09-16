1e25d8ff9b504f6c40515349c639d779f89ca2c6
Joel Jose <joeljose420@gmail.com>
2021-09-16 14:10:22 +0530

    update homepage

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './Header.css'
 import Button from '@material-ui/core/Button'
 import Logo from '../../assets/logo.svg'
 import {marginLeft} from '@material-ui/core/styles'
+import BookShow from '../../screens/bookshow/BookShow'
+
 
 
 
 function Header() {
+    let [loggedinflag, setLoggedinflag] = useState(false);
+    let [bookshowflag, setbookshowflag] = useState(false);
     return (
         <div className="Header">
             <img className="svgImage" src={Logo} alt="Logo" />
-            <Button style={{ marginLeft: '95%' }} variant="contained" color="default">Login</Button>
+            <div className="bookshowDiv">
+                {
+                    (bookshowflag) ?
+                        <Button variant="contained" color="primary" onClick={()=>{<BookShow baseUrl="/"></BookShow>}}>Book Show</Button> : null
+                }
+            </div>
+            <div className="loginlogoutDiv">
+                {
+                    (loggedinflag) ?
+                        <Button variant="contained" color="default">Logout</Button>
+                        :
+                        <Button variant="contained" color="default">Login</Button>
+
+                }
+            </div>
+
         </div>
     )
 }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,117 @@
 import React from 'react'
 import Header from '../../common/header/Header'
+import '../home/Home.css'
+import { makeStyles } from '@material-ui/core/styles';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import antmanimage from '../../assets/moviePosters/antman.jpeg'
+import spidermanimage from '../../assets/moviePosters/spiderman.jpg'
+
+let itemData = [
+    {
+        img: antmanimage,
+        title: 'AntMan',
+        author: 'Marvel'
+    },
+    {
+        img: spidermanimage,
+        title: 'SpiderMan',
+        author: 'Marvel'
+    },
+    {
+        img: antmanimage,
+        title: 'AntMan',
+        author: 'Marvel'
+    },
+    {
+        img: spidermanimage,
+        title: 'SpiderMan',
+        author: 'Marvel'
+    },
+    {
+        img: antmanimage,
+        title: 'AntMan',
+        author: 'Marvel'
+    },
+    {
+        img: spidermanimage,
+        title: 'SpiderMan',
+        author: 'Marvel'
+    },
+    {
+        img: antmanimage,
+        title: 'AntMan',
+        author: 'Marvel'
+    },
+    {
+        img: spidermanimage,
+        title: 'SpiderMan',
+        author: 'Marvel'
+    },
+    {
+        img: antmanimage,
+        title: 'AntMan',
+        author: 'Marvel'
+    }
+
+]
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      backgroundColor: theme.palette.background.paper,
+    },
+    imageList: {
+      flexWrap: 'nowrap',
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: 'translateZ(0)',
+    },
+    title: {
+      color: "white",
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+  }));  
 
 function Home() {
+    const classes = useStyles();
     return (
         <div>
             <Header></Header>
+            <div className="upcomingDiv">
+                <label>
+                    Upcoming Movies
+                </label>
+            </div>
+            <div className="moviegridDiv">
+            <div className={classes.root}>
+      <ImageList collHeight={250} className={classes.imageList} cols={6}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img src={item.img} alt={item.title} />
+            <ImageListItemBar
+              title={item.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              /*actionIcon={
+                <IconButton aria-label={`star ${item.title}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }*/
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </div>
+            </div>
         </div>
     )
 }
